61e9b8b2be46e211d27123d11464973605c1a3e7
unknown <methot.architecture@gmail.com>
2024-08-24 12:01:17 -0400

    :sparkles: Add Autjs example code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { SignIn } from "@auth/sveltekit/components"
+</script>
+
+<main>
+    <h1>SvelteKit Auth Example</h1>
+    <p>
+      This is an example site to demonstrate how to use <a
+        href="https://kit.svelte.dev/">SvelteKit</a
+      >
+      with <a href="https://sveltekit.authjs.dev">SvelteKit Auth</a> for authentication.
+    </p>
+    <div>
+      <SignIn provider="github" signInPage="signin" />
+    </div> 
+  </main>
+  
+  <style>
+    main {
+      flex: 1;
+    }
+  </style>
